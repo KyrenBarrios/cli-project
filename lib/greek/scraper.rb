cee86f7c657e0a6984a6a37c395aefe707136b34
Kyren Barrios <74169037+KyrenBarrios@users.noreply.github.com>
2020-12-05 17:54:58 -0800

    Update scraper.rb

--- lib/greek/scraper.rb
+++ lib/greek/scraper.rb
@@ -1,15 +1,16 @@
 class Greek::Scraper
   
     def self.scrape
-      @doc ||= Nokogiri::HTML(open("https://rickriordan.com/extra/meet-the-greek-gods/").css('div.q-and-a')
+      @doc ||= Nokogiri::HTML(open("https://rickriordan.com/extra/meet-the-greek-gods/")).css('div.q-and-a')
     end
     
     def self.scrape_immortals
         self.scrape.each do |god|
           name = god.search("div.question").text.strip
-          info = god.search("div.answer").text
-          Immortal.new(name, info)
+          desc = god.search("div.answer").text
+          Immortal.new(name, desc)
         end
       end
+   
   end
   
