a71591aba2b1b0653e104c58ccd6bbcffa61cfe4
Kyren Barrios <74169037+KyrenBarrios@users.noreply.github.com>
2020-12-05 17:53:54 -0800

    Update cli.rb

--- lib/greek/cli.rb
+++ lib/greek/cli.rb
@@ -1,6 +1,7 @@
 class Greek::CLI
   
     def call
+        puts "loading..."
         Greek::Scraper.scrape_immortals
       welcome
       display_immortals
@@ -9,40 +10,45 @@ class Greek::CLI
     end
     
     def welcome
-          puts ""
-          puts "----------------------------------------"
-          puts "                Welcome                 "
-          puts "----------------------------------------"
+        puts "Welcome to your lesson of Greek Mythology"
+        puts "-----------------------------------------"
     end
     
    def display_immortals
       Immortal.all.each.with_index(1) do |a, b|
         puts "#{b}.   #{a.name}"
       end
+      puts "----------------------------------------- e"
     end
-    
-    def display_info
-      Immortal.all.each.with_index(1) do |a, b|
-      end
-    end
-    
+
+
     def menu
-      input = nil
-      while input != "exit"
-        puts "Enter the number of the immortals you want more information on, type list to see list, or exit:"
-        input = gets.strip.downcase
-        
-       if input.to_i > 0 && input.to_i < 21
-          the_immortal = display_info[input.to_i-1]
-          puts "#{the_immortal.name} - #{the_immortal.info}"
-        elsif input == "list"
-            display_immortals
-        elsif input != "exit"
-          puts "Invalid response."
+        input = nil
+        while input != "exit"
+          puts "Enter the number of the god you want more information on, type 'list' to see list, or 'exit':"
+          input = gets.strip.downcase
+          
+         if input.to_i > 0 && input.to_i < 21
+            the_immortal = display_info[input.to_i-1]
+            puts "#{the_immortal.name} - #{the_immortal.desc}"
+          elsif input == "list"
+              display_immortals
+          elsif input != "exit"
+            puts "Invalid response."
+          end
         end
       end
-    end
+    
+    
+      def display_info
+      Immortal.all.each.with_index(1) do |a, b|
+      end
+      end
+    
+    
   
     def goodbye
-  puts "goodbye"
+  puts "Now you know more than you did yesterday, goodbye!"
     end
+
+end
